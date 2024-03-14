bb1056702331b945d344462fac5b5b315c9b0d81
chrispine owuor <owuorchrispine77@gmail.com>
2024-03-14 23:33:51 +0300

    reset pass mvp

--- src/components/Otp.js
+++ src/components/Otp.js
@@ -1,83 +1,37 @@
-import { useRef, useState } from "react";
+import React, { useState } from "react";
+import OtpInput from "react-otp-input";
 
-export default function Otp() {
-  const fieldsRef = useRef();
-  const [state, setState] = useState({
-    code1: "",
-    code2: "",
-    code3: "",
-    code4: "",
-  });
-
-  const inputFocus = (e) => {
-    const elements = fieldsRef.current.children;
-    const dataIndex = +e.target.getAttribute("data-index");
-    if (e.key === "Delete" || e.key === "Backspace") {
-      const next = dataIndex - 1;
-      if (next > -1) {
-        elements[next].focus();
-      }
-    } else {
-      const next = dataIndex + 1;
-      if (
-        next < elements.length &&
-        e.target.value !== " " &&
-        e.target.value !== "" &&
-        e.key.length === 1
-      ) {
-        elements[next].focus();
-      }
-    }
-  };
-
-  const handleChange = (e, codeNumber) => {
-    const value = e.target.value;
-    setState({ ...state, [codeNumber]: value.slice(value.length - 1) });
-  };
+export default function Otp({ getOtp, otpRes }) {
+  const [otp, setOtp] = useState("");
+  if (otp.length === 4) {
+    getOtp(otp);
+    setOtp("")
+  }
 
   return (
-    <div className="mt-3">
-      <p className="text-gray-600 text-center ">
-        Enter the Verification code sent to your email
-      </p>
-      <div ref={fieldsRef} className="mt-2 flex items-center gap-x-2 justify-center">
-        <input
-          type="text"
-          data-index="0"
-          placeholder="0"
-          value={state.code1}
-          className="w-12 h-12 rounded-lg border focus:border-indigo-600 outline-none text-center text-2xl"
-          onChange={(e) => handleChange(e, "code1")}
-          onKeyUp={inputFocus}
-        />
-        <input
-          type="text"
-          data-index="1"
-          placeholder="0"
-          value={state.code2}
-          className="w-12 h-12 rounded-lg border focus:border-indigo-600 outline-none text-center text-2xl"
-          onChange={(e) => handleChange(e, "code2")}
-          onKeyUp={inputFocus}
-        />
-        <input
-          type="text"
-          data-index="2"
-          placeholder="0"
-          value={state.code3}
-          className="w-12 h-12 rounded-lg border focus:border-indigo-600 outline-none text-center text-2xl"
-          onChange={(e) => handleChange(e, "code3")}
-          onKeyUp={inputFocus}
-        />
-        <input
-          type="text"
-          data-index="3"
-          placeholder="0"
-          value={state.code4}
-          className="w-12 h-12 rounded-lg border focus:border-indigo-600 outline-none text-center text-2xl"
-          onChange={(e) => handleChange(e, "code4")}
-          onKeyUp={inputFocus}
-        />
-      </div>
-    </div>
+    <>
+      {" "}
+      {!otpRes && (
+        <div>
+          <p className="text-center text-gray-600 ">
+            Enter otp code you receved in your email.If not received check spam
+            folder
+          </p>
+          <OtpInput
+            placeholder="0000"
+            skipDefaultStyles={true}
+            containerStyle={"flex justify-center"}
+            value={otp}
+            inputStyle={
+              "outline outline-1 rounded-sm mx-4 aspect-square h-12 text-4xl text-center "
+            }
+            onChange={setOtp}
+            numInputs={4}
+            renderSeparator={<span>-</span>}
+            renderInput={(props) => <input {...props} />}
+          />
+        </div>
+      )}
+    </>
   );
 }
