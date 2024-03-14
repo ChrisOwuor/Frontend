5988796580ed72f8ecf9033fc3906a3168abb96c
chrispine owuor <owuorchrispine77@gmail.com>
2024-03-14 23:33:51 +0300

    reset pass mvp

--- src/components/AlerttError.js
+++ src/components/AlerttError.js
@@ -1,33 +1,12 @@
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
+import { useState } from "react";
 import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
 import Collapse from "@mui/material/Collapse";
 
+
 export default function AlerttError({ msg }) {
   const [open, setOpen] = useState(true);
-  const isMounted = useRef(true);
-
-  useEffect(() => {
-    // Component is mounted
-    isMounted.current = true;
-
-    return () => {
-      // Component will unmount
-      isMounted.current = false;
-    };
-  }, []);
-
-  useEffect(() => {
-    if (isMounted.current) {
-      console.log("mount");
-      setTimeout(() => {
-        setOpen(false);
-      }, 2000);
-    } else {
-      setOpen(false);
-      console.log("unmount");
-    }
-  }, []);
 
   return (
     <Collapse in={open}>
