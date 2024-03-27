8962a04334c5e5824cb5ee0fa0a3700c93bd30ec
chrispine owuor <owuorchrispine77@gmail.com>
2024-03-27 20:11:26 +0300

    maps

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,4 +1,4 @@
-import {  useState } from "react";
+import { useState } from "react";
 
 import { NavLink, Outlet } from "react-router-dom";
 
@@ -7,9 +7,21 @@ import PersonAddAlt1RoundedIcon from "@mui/icons-material/PersonAddAlt1Rounded";
 import PersonSearchRoundedIcon from "@mui/icons-material/PersonSearchRounded";
 
 const subCategories = [
-  { name: "Add Missing Person", href: "/services/add_person" ,icon :<PersonAddAlt1RoundedIcon/>},
-  { name: "Find Missing Person", href: "/services/find_person",icon:<PersonSearchRoundedIcon/> },
-  { name: "Report A found person", href: "/services/report_person",icon:<StickyNote2RoundedIcon/> },
+  {
+    name: "Add Missing Person",
+    href: "/services/add_person",
+    icon: <PersonAddAlt1RoundedIcon />,
+  },
+  {
+    name: "Find Missing Person",
+    href: "/services/find_person",
+    icon: <PersonSearchRoundedIcon />,
+  },
+  {
+    name: "Report A found person",
+    href: "/services/report_person",
+    icon: <StickyNote2RoundedIcon />,
+  },
 ];
 
 export default function Example() {
@@ -20,13 +32,16 @@ export default function Example() {
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4  h-full ">
           <div className="hidden lg:block ">
             <div className=" top_div flex items-top  justify-between  mb-4 ">
-              <h1 className="text-3xl font-semibold tracking-tight text-gray-900">
+              <h1 className="text-2xl font-semibold tracking-tight text-gray-900">
                 {service}
               </h1>
             </div>
             <ul className="space-y-6  border-gray-200 pb-6 text-sm font-medium text-gray-900 ">
               {subCategories.map((category) => (
-                <li key={category.name} className="flex ">
+                <li
+                  key={category.name}
+                  className="flex bg-gray-100 rounded-lg pl-2"
+                >
                   {category.icon}
                   <NavLink
                     to={category.href}
