d54b6eb8e03922b81a0ef671972afdbffe40b1ca
chrispine owuor <owuorchrispine77@gmail.com>
2024-03-29 11:08:58 +0300

    res

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./App.css";
-// import Home from "./pages/Home";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
